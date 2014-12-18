5975ee6981f76fe0f35c0c76ef99df3a029865d5
Miguel Angel Torres <miguel@internetrepublica.com>
2014-12-18 18:05:36 +0100

    Mostrar información solo a usuarios que provengan de buscadores

---
+++ mostrar_info_user_from_buscadores.php
@@ -0,0 +1,7 @@
+<?php
+  if (preg_match('/q=|search/', $_SERVER['HTTP_REFERER']) ) {
+        print '';
+  }
+  //Dentro del print, meteremos el código que queramos, como puede ser
+  //un bloque de anuncios Adsense
+?>
